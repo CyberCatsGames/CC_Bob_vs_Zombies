a00a29bf4997f7acc20e41fed241ee773d2c56af
Our_bundle_of_joy <76647473+Daniyal-Akhadov@users.noreply.github.com>
2022-12-19 20:34:24 +0400

    [add] localization for text

---
+++ Assets/Suriyun/MobileTPS/Code/Localization/Language.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Suriyun.MobileTPS
+{
+    public class Language : MonoBehaviour
+    {
+        public static Language Instance;
+
+        public string CurrentLanguage;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+                CurrentLanguage = GetLanguage();
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        [DllImport("__Internal")]
+        private static extern string GetLanguage();
+    }
+}
