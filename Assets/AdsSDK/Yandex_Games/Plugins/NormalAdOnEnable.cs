42fb112b94fe9cea7c9f00acfc4bb09a1a5ac18e
Yurius <59869999+Yurius-Softable@users.noreply.github.com>
2022-12-20 15:35:38 +0400

    Added PopUp Normal Ad

---
+++ Assets/AdsSDK/Yandex_Games/Plugins/NormalAdOnEnable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalAdOnEnable : MonoBehaviour
+{
+    private bool isStart = true;
+    private void Start() {
+        if (MyYandex.Instance) {
+            isStart = false;
+            MyYandex.Instance.ShowNormalAD_Intern();
+        }
+    }
+
+    private void OnEnable() {
+        if (MyYandex.Instance && isStart == false) {
+            MyYandex.Instance.ShowNormalAD_Intern();
+        }
+    }
+}
